246b764efeec101854d84b53737d7053f7916aae
Abda Shaffan <abdashaffan@gmail.com>
2019-08-12 01:33:14 +0700

    use material ui for text field

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
 const Input = ({ handleAppChange }) => {
   const [input, setInput] = useState("");
@@ -8,12 +10,32 @@ const Input = ({ handleAppChange }) => {
     handleAppChange(e.target.value);
   };
 
+  const useStyles = makeStyles(theme => ({
+    container: {
+      display: "flex",
+      flexWrap: "wrap"
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200
+    },
+    dense: {
+      marginTop: 19
+    },
+    menu: {
+      width: 200
+    }
+  }));
+  const classes = useStyles();
   return (
-    <input
-      type="text"
-      placeholder="Nama/NIM"
-      name="input"
+    <TextField
+      id="name"
+      label="Nama/NIM"
+      className={classes.textField}
+      value={input}
       onChange={handleChange}
+      margin="normal"
     />
   );
 };
