adcf1d289ed3bfc9d51f862efc24ce972ededcb0
Abda Shaffan <abdashaffan@gmail.com>
2019-08-15 22:45:26 +0700

    fix typo on server

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 const studentRoute = require("./server/routes/students");
 app.use("/api/students", studentRoute);
 
-if (process.env.NODE_NEV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
