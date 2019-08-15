06a9827b293583f58f54753f56fc0cf5793ca453
Abda Shaffan <abdashaffan@gmail.com>
2019-08-15 13:37:52 +0700

    Refactor table imports

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,15 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableHead from "@material-ui/core/TableHead";
-import TableCell from "@material-ui/core/TableCell";
-import TableFooter from "@material-ui/core/TableFooter";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import Hidden from "@material-ui/core/Hidden";
+import {Table,TableBody,TableHead,TableCell,TableFooter,TablePagination,TableRow,Paper,Hidden} from "@material-ui/core";
 import PageNumber from "./PageNumber";
 import Loader from "./Loader";
 
