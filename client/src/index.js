8a53dceb6adbbc75aa89784975aa3cfb747519cd
Abda Shaffan <abdashaffan@gmail.com>
2019-08-18 18:10:59 +0700

    delete empty css script

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-// import WebFont from 'webfontloader';
-import "./index.css";
 import * as serviceWorker from './serviceWorker';
 
-
-// WebFont.load({
-//     google: {
-//         families: ['Titillium Web:300,400,700', 'sans-serif']
-//     }
-// });
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
